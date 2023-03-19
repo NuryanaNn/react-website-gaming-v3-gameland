7a5d52314914aaef9730c329faaad7f634a4c1c6
Nuryana N <nuryan504@gmail.com>
2023-03-19 19:32:31 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './style/style.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
+import Popular from './components/Popular'
+import Release from './components/Release'
+import Category from './components/Category'
+import Watch from './components/Watch'
+import News from './components/News'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Navbar'>
+      <Navbar/>
+    </div>
+    <div className="Hero">
+      <Hero/>
+    </div>
+    <div className="Popular">
+      <Popular/>
+    </div>
+    <div className="Release">
+      <Release/>
+    </div>
+    <div className="Category">
+      <Category/>
+    </div>
+    <div className="Watch">
+      <Watch/>
+    </div>
+    <div className="News">
+      <News/>
+    </div>
+    <div className="Footer">
+      <Footer/>
+    </div>
     </div>
   );
 }
